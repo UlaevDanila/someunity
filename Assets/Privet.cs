581823d34ed9957c83926b7cddfbfee3a862d33e
COMPUDAHTER\Данила <ulaevdanila63@gmail.com>
2021-09-02 21:35:32 +0300

    dfjasfjksaflasas

---
+++ Assets/Privet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Privet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody _playerBody;
+
+    private void Start()
+    {
+        _playerBody = GetComponent<Rigidbody>();    
+    }
+
+    private void Update()
+    {
+        var input = Input.GetAxis("Horizontall");
+
+        Debug.Log(input);
+
+        var direction = new Vector3(0f, 0f, input);
+        _playerBody.velocity = direction * input;
+    }
+}
